20e9d27970032da84c09919aa6e1c504f6885dae
yidianhengji <493871850@qq.com>
2020-12-25 17:39:23 +0800

    first commit

---
+++ src/layout/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navbar">
+    23
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style lang="sass" scoped>
+.navbar
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0,21,41,.08);
+</style>
---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="sidebar-container">
+    Sidebar
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar"
+}
+</script>
+
+<style lang="sass" scoped>
+.sidebar-container
+  transition: width .28s;
+  width: 210px!important;
+  background-color: #304156;
+  height: 100%;
+  position: fixed;
+  font-size: 0;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1001;
+  overflow: hidden;
+</style>
