500644e8f6e6155812de7d6e5d458573362257ba
yidianhengji <493871850@qq.com>
2021-01-21 17:35:00 +0800

    formbase

---
+++ src/views/formBase/tableOne/tableOne.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-full">
+    <basics-table></basics-table>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TableOne'
+})
+</script>
+
+<style scoped>
+
+</style>
